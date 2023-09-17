ee5af8729be355b502266ac90139398dd51efdda
machacaleila <mleila@byui.edu>
2023-09-17 00:20:29 -0700

    changes prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        Console.Write("What is your Last Name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {name} {last}.");
+        
     }
 }

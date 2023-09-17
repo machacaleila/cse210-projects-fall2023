677b96531cdf72217901dd2b3313a69b2a61f42f
machacaleila <mleila@byui.edu>
2023-09-17 01:30:59 -0700

    Week01

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Hey there! Do you wanna know your letter grade? ");
+        Console.Write("Please tell me your final results: ");
+        string finalResult = Console.ReadLine();
+        int number = int.Parse(finalResult);
+
+        string letter = " ";
+
+        if (number >= 90)
+        {
+           letter = "A"; 
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (number >= 70 )
+        {
+            Console.WriteLine("You passed the course!");
+        }
+        else {
+            Console.WriteLine("You can do it better next time!");
+        }
+
     }
 }

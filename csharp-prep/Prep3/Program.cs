a3bd4053b8874dd7f8dafc6995e814c9c2ba3c16
machacaleila <mleila@byui.edu>
2023-10-11 01:09:51 -0700

    finshed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using static System.Console;
 
 class Program
 {
@@ -12,22 +13,23 @@ class Program
         Random rand = new Random();
         int number = rand.Next(0, 100);
         int numberguess = 1;
+        int numberOfTries = 0;
         while (number != numberguess){
             Console.Write("What is your guess? ");
             //Ask question to the user
             numberguess = int.Parse(Console.ReadLine());
             if (number > numberguess){
                 Console.WriteLine("higher");
+                numberOfTries++;
                 }
             else if (number < numberguess){
                 Console.WriteLine("Lower");
+                numberOfTries++;
                 }
             else{
                 Console.WriteLine("Congrats! You guessed it"); 
-                numberguess++; 
-                Console.WriteLine($"You guessed it {numberguess} times.");
-                //string answer = "yes";
-                //while (answer !="no"){
+                Console.WriteLine($"You guessed it in {numberOfTries} times.");
+    
                 Console.WriteLine("Dou you want to keep playing ?");
                 string answer = Console.ReadLine();
                 if (answer== "yes"){
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,51 @@
 using System;
-
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>(); 
+        Console.WriteLine("Hi there! Add any number to this list and type 0 when you finished. ");
+        
+        int rule = 2; 
+        while (rule != 10){
+
+            Console.Write("Enter a number: ");
+            rule = int.Parse(Console.ReadLine());
+            numbers.Add(rule);
+           // foreach(int other in numbers){
+                //Console.WriteLine(other);
+            //}
+            if (rule == 0){
+
+                int total = numbers.Sum();
+                Console.WriteLine($"The sum is: {total}");
+                double average = numbers.Average();
+                Console.WriteLine($"The average is: {average}");
+                int maxNum = numbers.AsQueryable().Max();
+                Console.WriteLine($"The largest number is : {maxNum}");
+                int minNum = numbers.AsQueryable().Min();
+                int a = -1;
+                Console.WriteLine($"The smallest positive number is: {minNum*a}");
+                //Sort List
+                numbers.Sort();
+                Console.WriteLine("The sorted list is: ");
+                for (int i = 0; i<numbers.Count; i++){
+                    int sort = numbers[i];
+                    Console.WriteLine(sort);
+                }
+                
+                break;
+            }
+            else{
+                rule = 5;
+            }
+
+        }
+
+        
+
     }
 }

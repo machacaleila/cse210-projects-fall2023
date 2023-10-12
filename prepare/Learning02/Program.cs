c37edfe97377137fcdab1511ddad016c601ee4f2
machacaleila <mleila@byui.edu>
2023-10-12 00:58:00 -0700

    try two

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Esta es solo un trabajador vamos a usar la clase en solo 1 trabajaodr
+        Job job1 = new Job();//asi llamo la clase no tengo que agregar como el el programa 5. Aca solo tipear dos veces el titulo
+        job1._jobTitle = "Software Engineer";
+        job1.DisplayInfo();//Asi imprimo, llamando a Display junto con en este caso la perosna job1
     }
 }

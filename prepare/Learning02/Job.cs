5cd3062a3ee3695a4882fe2acac84b4f2bdc608d
machacaleila <mleila@byui.edu>
2023-10-12 00:57:09 -0700

    try one

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Text; 
+
+
+public class Job 
+{
+    
+    public string _jobTitle;//No agreagar comillas ni nada porque solo va a ser una caja vacia.Template
+    public string _companyName;
+    public int _startYear;
+    public int _endYear;
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"{_jobTitle}");
+    }
+   
+}

783ce8c56a3bca73df586ab1a88633eb97451a8e
machacaleila <mleila@byui.edu>
2023-10-13 19:54:09 -0700

    I finished

--- prepare/Learning03/Entry.cs
+++ prepare/Learning03/Entry.cs
@@ -6,11 +6,11 @@ using System.IO;
 public class Entry
 {
     public string _userResponse;
-    public DateTime theCurrentTime = DateTime.Now; 
+        
+    public string _theCurrentTime = DateTime.Now.ToShortDateString(); 
     public string _prompt;
     public void DisplayEntries(){
-        string _dateText = theCurrentTime.ToShortDateString();
-        Console.WriteLine($"Date: {_dateText} - Prompt: {_prompt} \n{_userResponse}");
+        Console.WriteLine($"Date: {_theCurrentTime} - Prompt: {_prompt} \n{_userResponse}");
     }
 
 }

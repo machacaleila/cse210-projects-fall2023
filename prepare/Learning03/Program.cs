9558feff4bef673263c6cd1ff10db760773b222f
machacaleila <mleila@byui.edu>
2023-10-13 19:55:41 -0700

    I fnished

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,18 +5,8 @@ class Program
 {
     static void Main(string[] args){
           
-        Entry answer = new Entry();
-
-        Random questions = new Random();
-        int x = questions.Next(5);
-        string[] array = new string[5];
-        array[0] = "Who was the most interesting person I interacted with today?";
-        array[1] = "What was the best part of my day?";
-        array[2] = "How did I see the hand of the Lord in my life today?";
-        array[3] = "What was the strongest emotion I felt today?";
-        array[4] = "If I had one thing I could do over today, what would it be?";
-
-        
+        Journal load = new Journal();
+//I am going to make the questions here.
         List<string> menu = new List<string>();//Create the list menu
         menu.Add("1.Write");
         menu.Add("2.Display");
@@ -24,7 +14,7 @@ class Program
         menu.Add("4.Save");
         menu.Add("5.Quit");
         //Print the whole menu
-        //I am going to make the questions here.
+        
         Console.WriteLine("Welcome to the journal Program!");
 //Use loop to stop the menu and sking to continue
         int rule = 5;
@@ -38,18 +28,21 @@ class Program
              Console.Write("What would you like to do?:  ");
              rule = int.Parse(Console.ReadLine());
              if (rule == 1){
-                Console.WriteLine(array[x]);  
-                answer._userResponse = Console.ReadLine();
+               load.DisplayQuestions();
+                  
              }
              else if (rule == 2){
-                answer._prompt = array[x];
-                answer.DisplayEntries();
+                load.ShowAnswers();
+               
              }
              else if (rule == 3){
-                Console.WriteLine("not yet...");
+                load.loadText(); 
+         
              }
              else if (rule == 4){
-                Console.WriteLine("Almost. . .");
+                Console.Write("What is the file name? ");
+                load.DisplayJournal();
+               
              }
              else{
                 Console.WriteLine("Thanks for waiting! I am not smart and fast.");
